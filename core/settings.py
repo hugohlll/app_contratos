d6a976732830e8aaba91e55c8aa778532295f754
Hugo Lima <iaentusiasta@proton.me>
2026-02-06 17:45:10 -0300

    Fix template rendering errors & format contract value
    Enabled pt-br localization. formatted contract value to R$ X.XXX,XX. Fixed split template tags system-wide.

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'contratos',  # <--- Adicione esta linha
 ]
 
@@ -108,13 +109,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
 USE_TZ = True
+
+USE_THOUSAND_SEPARATOR = True
 
 
 # Static files (CSS, JavaScript, Images)
